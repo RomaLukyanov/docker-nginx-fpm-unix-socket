88b56d7746eaa55bbaf81ef2a200e2d0257121e2
Roma Lukyanov <romaymniy@gmail.com>
2023-08-16 16:11:38 +0300

    Config work on Ubuntu

---
+++ code/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Привет, Otus!<br>".date("Y-m-d H:i:s") ."<br><br>";
+	
+// $mysqli = mysqli_connect("db", "root", "123", "university");
+// $result = mysqli_query($mysqli, "SHOW TABLES");
+// var_dump($result->fetch_array());
+
+//echo "Новая строка";
+
+phpinfo();
